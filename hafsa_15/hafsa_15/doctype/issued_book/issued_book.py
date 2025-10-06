73c37528231bb31acfe2933ab4510f838009af17
hafsa-rajpoot <faisal-zahoor>
2025-10-06 13:05:05 +0500

    fix changes

--- hafsa_15/hafsa_15/doctype/issued_book/issued_book.py
+++ hafsa_15/hafsa_15/doctype/issued_book/issued_book.py
@@ -1,13 +1,18 @@
 # Copyright (c) 2025, Nextash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class IssuedBook(Document):
     def validate(self):
         book = frappe.get_doc("Library Book", self.book)
+
+        # Check if the book is available for issuing
         if book.status != "Available":
             frappe.throw(f"Book '{book.title}' is not available for issuing. Current status: {book.status}")
 
+        # Optional: double-check if status is already "Issued"
+        if book.status == "Issued":
+            frappe.throw(f"The book '{book.title}' is already issued.")
--- hafsa_15/hafsa_15/doctype/library_member/library_member.py
+++ hafsa_15/hafsa_15/doctype/library_member/library_member.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, Nextash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryMember(Document):
-	pass
+    def validate(self):
+       if frappe.db.exists("Customer", {"library_card_number": self.library_card_number, "name": ["!=", self.name]}):
+        frappe.throw("Library Card Number must be unique")
